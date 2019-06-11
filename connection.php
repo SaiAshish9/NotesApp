7c228dbe0eee7d7f0b02ff576053efd44dee45dc
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-06-11 19:32:50 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+$link = mysqli_connect("localhost", "Sai", "sai", "notes");
+if(mysqli_connect_error()){
+    die('ERROR: Unable to connect:' . mysqli_connect_error());
+    echo "<script>window.alert('Hi!')</script>";
+}
+    ?>
+
+<!-- heroku addons:create cleardb:ignite
+
+
+heroku config | grep CLEARDB_DATABASE_URL
+CLEARDB_DATABASE_URL => mysql://adffdadf2341:adf4234@us-cdbr-east.cleardb.com/heroku_db?reconnect=true
+Copy the value of the CLEARDB_DATABASE_URL config variable.
+
+
+heroku config:set DATABASE_URL='mysql://adffdadf2341:adf4234@us-cdbr-east.cleardb.com/heroku_db?reconnect=true'
+Adding config vars:
+DATABASE_URL => mysql2://adffd...b?reconnect=true
+Restarting app... done, v61. -->
